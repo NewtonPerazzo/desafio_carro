d8346b0b851e74d4120ace40f595f30a78bf4b76
newtonperazzo <perazzoneto99@hotmail.com>
2020-09-01 19:31:54 -0300

    Question 17

---
+++ ListExercises/17.py
@@ -0,0 +1,28 @@
+info_atleta = []
+lista_requisitos = ["Nome do atleta: ",
+                    "Primeiro Salto: ",
+                    "Segundo Salto: ",
+                    "Terceiro Salto: ",
+                    "Quarto Salto: ",
+                    "Quinto Salto: "]
+while True:
+    name = str(input("Nome: "))
+    if name == '':
+        break
+    else:
+        info_atleta.append(name)
+        for s in range(5):
+            resp = float(input(f"Informe o salto {s+1}: "))
+            info_atleta.append(resp)
+        cont = 0
+        for i in info_atleta[1:]:
+            cont += 1
+            print(f'Salto {cont}: ', i)
+        print('\nResultado final: \n'
+            f'Atleta: {info_atleta[0]}\n'
+            f'Saltos: {info_atleta[1:]}\n'
+            f'Média dos saltos: {sum(info_atleta[1:])/len(info_atleta[1:])} m')
+
+
+
+
